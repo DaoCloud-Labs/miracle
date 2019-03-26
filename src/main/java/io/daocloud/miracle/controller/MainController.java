07b8a26e2ec366242951c8889c51655f0cb76b94
zhuohao.li <zhuohao.li@daocloud.io>
2019-03-26 18:24:46 +0800

    fix
    返回图片的base64编码

--- src/main/java/io/daocloud/miracle/controller/MainController.java
+++ src/main/java/io/daocloud/miracle/controller/MainController.java
@@ -1,18 +1,32 @@
 package io.daocloud.miracle.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.io.IOException;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import sun.misc.BASE64Encoder;
 
-@Controller
+@RestController
 public class MainController {
 
-  @RequestMapping("/")
-  public String one(){
-    return "index_a.html";
+  @GetMapping(value = "/index", produces = MediaType.IMAGE_JPEG_VALUE)
+  @ResponseBody
+  public String indexA() throws IOException {
+    Resource cpr = new ClassPathResource("static/indexA.jpg");
+    //for test
+    BASE64Encoder encoder = new BASE64Encoder();
+    return encoder.encode(FileCopyUtils.copyToByteArray(cpr.getInputStream()));
   }
-
-  @RequestMapping("/another")
-  public String two(){
-    return "index_b.html";
-  }
+//
+//  @GetMapping(value = "/indexB", produces = MediaType.IMAGE_JPEG_VALUE)
+//  @ResponseBody
+//  public byte[] indexB() throws IOException {
+//    Resource cpr = new ClassPathResource("static/indexB.jpg");
+//    //for test
+//    return FileCopyUtils.copyToByteArray(cpr.getInputStream());
+//  }
 }
